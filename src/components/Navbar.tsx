eaa5ca4fcce387228caada9bb2fff1750c00ad4f
Novekfreelance <novekfreelance.github@gmail.com>
2023-09-24 14:40:39 +0100

    fixation du bug de l'animation de la navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,35 @@
-"use client"
+"use client";
 import Link from "next/link";
 import Logo from "./ui/Logo";
 import { NavLinks, AuthLinks } from "@/components/Link";
-import ScrollListener from "./ScrollListener";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type Props = {
   className?: string;
 };
 
+let lastScrollTop = 0;
+
 const Navbar: React.FC<Props> = ({ className }) => {
   const [scrollingDown, setScrollingDown] = useState(false);
 
-  // Cette fonction gère le scroll
-  const handleScroll = (isScrollingDown: boolean) => {
-    setScrollingDown(isScrollingDown);
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      if (scrollTop > lastScrollTop) {
+        setScrollingDown(true);
+      } else {
+        setScrollingDown(false);
+      }
+      lastScrollTop = scrollTop; // Mettre à jour la position précédente
+    };
+
+    window.addEventListener("scroll", handleScroll); // Écouter l'événement de défilement lorsque le composant est monté
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll); // Nettoyer l'écouteur d'événements lorsque le composant est démonté
+    };
+  }, []);
 
   const navbarClasses = `${className} navbar fixed left-2/4 z-[99] -translate-x-2/4 top-0 bg-white w-11/12 my-7 mx-auto max-w-screen-xl flex items-center h-20 rounded border border-solid shadow border-shadowColor transition-transform ${
     scrollingDown ? "translate-y-[-110px] transition-transform" : ""
@@ -37,7 +51,6 @@ const Navbar: React.FC<Props> = ({ className }) => {
           <AuthLinks />
         </ul>
       </div>
-      <ScrollListener onScroll={handleScroll} />
     </nav>
   );
 };
