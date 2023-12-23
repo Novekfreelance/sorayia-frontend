792b5fe79c71f9fa6f9deefb04e8a5ad69b78808
SergeCodeur <amoussougboserge@gmail.com>
2023-12-23 07:33:47 +0100

    Ajout de la page content

--- src/app/(dashboard-folders)/dashboard/content/[id]/page.tsx
+++ src/app/(dashboard-folders)/dashboard/content/[id]/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import ImportWebLinkCard from "@/components/dashboard/contentPage/ImportWebLinkCard"
+import UploadContentCard from "@/components/dashboard/contentPage/UploadContentCard"
+import WriteToContentCard from "@/components/dashboard/contentPage/WriteToContentCard"
+export const blockBoderStyle = "p-5 shadow rounded border-solid border border-shadowColor bg-white"
+
+
+const ContentFolferOption = () => {
 	return (
-		<div>FolderData</div>
+      <div className="w-full grid grid-cols-3 gap-5 p-3">
+				<WriteToContentCard />
+				<UploadContentCard/>
+				<ImportWebLinkCard />
+			</div>
 	)
 }
 
-export default page
+export default ContentFolferOption
