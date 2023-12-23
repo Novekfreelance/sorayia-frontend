a64c4caac3219edef2cdc34c13f57a87d18d2543
SergeCodeur <amoussougboserge@gmail.com>
2023-12-23 17:32:25 +0100

    modification : modification du composant client en composant server

--- src/app/(dashboard-folders)/dashboard/layout.tsx
+++ src/app/(dashboard-folders)/dashboard/layout.tsx
@@ -1,14 +1,5 @@
-"use client";
-import { Montserrat } from "next/font/google";
 import { ReactNode, FC } from "react";
 import Sidebar from "@/components/dashboard/Sidebar";
-
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400", "500", "600"],
-  style: ["normal"],
-});
 
 type DashboardLayoutProps = {
   children: ReactNode;
@@ -17,7 +8,7 @@ type DashboardLayoutProps = {
 const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
+      <body>
         <main className="h-full-dvh flex w-full">
           <Sidebar />
           <section className="flex-1 bg-gray-50 overflow-y-auto">{children}</section>
