43238d11b66e236a338bc9f0c1f3feb6f3e59ebb
SergeCodeur <amoussougboserge@gmail.com>
2023-12-23 07:49:34 +0100

    Mise à jour du hook personnaliser de la sidebar

--- src/hooks/useSidebarWidthEffect.ts
+++ src/hooks/useSidebarWidthEffect.ts
@@ -4,15 +4,18 @@ import { DEFAULT_SIDEBAR_WIDTH } from "@/constants/Sidebar/DefaultWidthConstant"
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
+
 const useSidebarWidthEffect = () => {
   const pathname = usePathname();
   const defaultSidebarWidth = DEFAULT_SIDEBAR_WIDTH;
   const { setSidebarWidth } = useSidebarStore();
-  
-  const profilPagePathname = "/dashboard/profil";
-  const botPagePathname = "/dashboard/bot";
+  const profilPageUrl = "/dashboard/profil";
+  const botPageUrl = "/dashboard/bot";
+  const settingsPageUrl = "/dashboard/setting";
+  const contentUrlMatch = pathname.match(/\/dashboard\/content\/(.*)/);
+  const contentUrl = contentUrlMatch ? contentUrlMatch[1] : null;
 
-  const pathnameMatch = pathname === profilPagePathname || pathname === botPagePathname;
+  const pathnameMatch = pathname === profilPageUrl || pathname === botPageUrl || contentUrl || pathname === settingsPageUrl;
   useEffect(() => {
     if (pathnameMatch) {
       setSidebarWidth(90);
