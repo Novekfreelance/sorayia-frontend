dc6f7087df02abafa6398e23b75411370e078d4b
SergeCodeur <amoussougboserge@gmail.com>
2023-12-23 07:40:47 +0100

    intégration du composant bouton d'ouverture de dossiers

--- src/components/dashboard/contentPage/OpenFolderBtn.tsx
+++ src/components/dashboard/contentPage/OpenFolderBtn.tsx
@@ -1,20 +1,20 @@
 import { Button } from "@/components/ui/button";
-// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { FC } from "react";
 
 type OpenFolderBtnProps = {
-	ChatId: string;
+	FolderId: string;
 }
-const OpenFolderBtn: FC<OpenFolderBtnProps> = ({ChatId}) => {
-	// const router = useRouter();
+const OpenFolderBtn: FC<OpenFolderBtnProps> = ({FolderId}) => {
+	const router = useRouter();
 	return (
 		<Button
           className="py-2 px-4 bg-primary"
           onClick={() => {
 						console.log(
-							`Opening conversation: ${ChatId}`
+							`Opening conversation: ${FolderId}`
 						);
-            // router.push(`/dashboard/content/${ChatId}`);
+            router.push(`/dashboard/content/${FolderId}`);
           }}
         >
           Open
