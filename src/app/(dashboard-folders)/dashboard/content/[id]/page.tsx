618551099aabf04a3628de3721012cfb69506a6b
SergeCodeur <amoussougboserge@gmail.com>
2023-12-23 17:51:42 +0100

    Correction : correction du type d'exportation et mise à jour en server component

--- src/app/(dashboard-folders)/dashboard/content/[id]/page.tsx
+++ src/app/(dashboard-folders)/dashboard/content/[id]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import ImportWebLinkCard from "@/components/dashboard/contentPage/ImportWebLinkCard";
 import UploadContentCard from "@/components/dashboard/contentPage/UploadContentCard";
 import WriteToContentCard from "@/components/dashboard/contentPage/WriteToContentCard";
@@ -11,7 +9,7 @@ export type ContentFolderProps = {
   };
 }
 
-export const ContentFolferOption: FC<ContentFolderProps> = ({params: { id }}) => {
+const ContentFolferOption: FC<ContentFolderProps> = ({params: { id }}) => {
   return (
     <div className="w-full grid grid-cols-3 gap-5 p-3">
       <WriteToContentCard />
