6fc17f7c8d65f7280e6382617aeac170fc549362
SergeCodeur <amoussougboserge@gmail.com>
2023-12-23 07:39:36 +0100

    Intégration du la card d'import de lien venant d'autres sites

---
+++ src/components/dashboard/contentPage/ImportWebLinkCard.tsx
@@ -0,0 +1,15 @@
+import { blockBoderStyle } from "@/app/(dashboard-folders)/dashboard/content/[id]/page";
+import { LinkIcon } from "@/components/icons/SvgIcons";
+import { cn } from "@/lib/utils";
+
+const ImportWebLinkCard = () => {
+  return (
+    <div className={cn(`${blockBoderStyle} space-y-3 cursor-not-allowed bg-gray-100`)}>
+      <LinkIcon width={45} height={45} fill="#00000099" />
+      <h3 className="text-2xl text-gray-300">Import</h3>
+      <p className="text-base-400 text-gray-300">Webpage with text content</p>
+    </div>
+  );
+};
+
+export default ImportWebLinkCard;
