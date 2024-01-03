c250e8f5dbe85db8e40fa7f9af97b49363ed95a0
SergeCodeur <amoussougboserge@gmail.com>
2024-01-03 16:08:28 +0100

    feat: ajout des datas  de la page content

---
+++ src/app/(dashboard-folders)/dashboard/content/[id]/FilesData.tsx
@@ -0,0 +1,63 @@
+import { FilesDataProps, columns } from "./columns";
+import { FilesDataTable } from "./data-table";
+
+async function getFilesData(): Promise<FilesDataProps[]> {
+  // Fetch data from your API here.
+  return [
+			{
+				id: "728ed52f",
+				name: "Agrosan",
+				type: "Files",
+				createDate: "2023-10-05T00:27:24.379Z",
+			},
+			{
+				id: "0oshhh22",
+				name: "Agrosan",
+				type: "Files",
+				createDate: "2022-10-05T00:27:24.379Z",
+			},
+			{
+				id: "668ed52f",
+				name: "Agrosan",
+				type: "Files",
+				createDate: "2021-11-05T00:27:24.379Z",
+			},
+			{
+				id: "668ed52f",
+				name: "Agrosan",
+				type: "Files",
+				createDate: "2024-10-04T00:27:24.379Z",
+			},
+			{
+				id: "668ed52f",
+				name: "Agrosan",
+				type: "Files",
+				createDate: "2024-10-03T00:27:24.379Z",
+			},
+			{
+				id: "668ed52f",
+				name: "Agrosan",
+				type: "Files",
+				createDate: "2024-10-02T00:27:24.379Z",
+			},
+			{
+				id: "668ed52f",
+				name: "Agrosan",
+				type: "Files",
+				createDate: "2024-10-01T00:27:24.379Z",
+			},
+			{
+				id: "668ed52f",
+				name: "Agrosan",
+				type: "Files",
+				createDate: "2024-10-01T00:27:24.379Z",
+			},
+  ];
+}
+
+const FilesData = async () => {
+	const data = await getFilesData();
+	return <FilesDataTable columns={columns} data={data} />;
+}
+
+export default FilesData
