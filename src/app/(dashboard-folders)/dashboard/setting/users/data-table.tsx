854e0f6418473cb494b22f6f18940268ba2b3d32
SergeCodeur <amoussougboserge@gmail.com>
2024-01-03 16:14:26 +0100

    feat: ajout des tables de la page users

---
+++ src/app/(dashboard-folders)/dashboard/setting/users/data-table.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function UsersDataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageIndex: 0,
+        pageSize: 3,
+      },
+    },
+  });
+
+  const showPagination = table.getRowModel().rows?.length > 2 && table.getState().pagination.pageIndex === 0 || table.getState().pagination.pageIndex > 0;
+
+  return (
+    <div className="flex flex-col justify-between min-h-[336px]">
+      <div>
+        {table.getRowModel().rows?.length ? (
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id} className="hover:bg-inherit p-2 border-x-0 border-t-0">
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead
+                      key={header.id}
+                      className="text-left"
+                      >
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody className="[&_tr:last-child]:border-y">
+              {table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  className="hover:bg-inherit border-x-0"
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="py-3 px-4 text-left">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        ) : (
+          <div className="h-full flex items-center justify-center min-h-[336px]">
+            <h2 className="text-2xl text-primary">
+							No users!
+            </h2>
+          </div>
+        )}
+      </div>
+      {showPagination ? (
+        <div className="flex items-center justify-center space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
