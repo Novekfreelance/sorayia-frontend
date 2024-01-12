15b4b49fa7782e5dffa6357f890d8e9f9c96accf
SergeCodeur <amoussougboserge@gmail.com>
2024-01-12 21:58:16 +0100

    feat: ajout de la logique l'affichage de l'id du fichier ouvert

--- src/components/dashboard/contentPage/FolderTitle.tsx
+++ src/components/dashboard/contentPage/FolderTitle.tsx
@@ -1,6 +1,12 @@
-const FolderTitle = () => {
+import { FC } from "react"
+
+type FolderTitleProps = {
+	id: string;
+}
+
+const FolderTitle: FC<FolderTitleProps> = ({id}) => {
 	return (
-		<h3 className="text-xl-500 text-primary">Folder&nbsp;</h3>
+		<h3 className="text-xl-500 text-primary">Folder&nbsp;{id}</h3>
 	)
 }
 
