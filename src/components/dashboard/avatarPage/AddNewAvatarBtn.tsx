22d4c278a5b72f7fb4148629ba6a35714d8f5552
SergeCodeur <amoussougboserge@gmail.com>
2024-01-12 21:40:01 +0100

    feat: ajout de la logique de création de bot

--- src/components/dashboard/avatarPage/AddNewAvatarBtn.tsx
+++ src/components/dashboard/avatarPage/AddNewAvatarBtn.tsx
@@ -1,15 +1,17 @@
 "use client";
-
 import { PlusIcon } from "@/components/icons/SvgIcons";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const AddNewAvatarBtn = () => {
-  const handleClick = () => {
-    console.log("clicked");
-  };
+  const router = useRouter();
+  const onClick = () => {
+    router.push('/dashboard/avatar/create-avatar');
+    router.refresh();
+  }
   return (
-    <Button className={cn("gap-2")} size={"lg"} onClick={handleClick}>
+    <Button className={cn("gap-2")} size={"lg"} onClick={onClick}>
       <PlusIcon fill="#ffffff" width={24} height={24} />
       Add new
     </Button>
