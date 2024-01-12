a8abfe75e508dcb3258ad612c8cb9a8eb46caab7
SergeCodeur <amoussougboserge@gmail.com>
2024-01-12 22:32:57 +0100

    feat: ajout de la logique d'affichage du nom de l'utilisateur

--- src/components/dashboard/profil/UserAvatar.tsx
+++ src/components/dashboard/profil/UserAvatar.tsx
@@ -1,14 +1,20 @@
+"use client";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import useUserStore from "@/app/store/AuthStore";
+
 
 type UserAvatarProps = {
   className?: string;
 };
 
 const UserAvatar = ({ className }: UserAvatarProps) => {
+  const user = useUserStore((state) => state.user);
   return (
     <Avatar className={`${className}`}>
       {/* <AvatarImage src="" /> */}
-      <AvatarFallback>JD</AvatarFallback>
+      <AvatarFallback>
+        {user && user.username.slice(0, 2).toUpperCase()}
+      </AvatarFallback>
     </Avatar>
   );
 };
