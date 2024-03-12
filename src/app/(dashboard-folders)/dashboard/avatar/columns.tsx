0c5b371618e5e7833920feee13881bcc2d382d57
SergeCodeur <amoussougboserge@gmail.com>
2024-03-12 20:49:55 +0100

    refactor : changement de nom de variable

--- src/app/(dashboard-folders)/dashboard/avatar/columns.tsx
+++ src/app/(dashboard-folders)/dashboard/avatar/columns.tsx
@@ -7,18 +7,17 @@ import { ColumnDef } from "@tanstack/react-table";
 
 export type AvatarDataProps = {
   id: string;
-  AvatarName: string;
-	avatar: string;
-	createDate: string;
+  name: string;
+  url: string;
 };
 
 export const columns: ColumnDef<AvatarDataProps>[] = [
   {
-    accessorKey: "AvatarName",
+    accessorKey: "name",
     header: "Name",
   },
-	{
-    accessorKey: "avatar",
+  {
+    accessorKey: "url",
     header: "Avatar",
     cell: ({ row }) => {
       const AvatarData = row.original;
@@ -27,26 +26,26 @@ export const columns: ColumnDef<AvatarDataProps>[] = [
           <Avatar className="w-28 h-28 !rounded">
             <AvatarImage
               className="!rounded"
-              src={AvatarData.avatar}
+              src={AvatarData.url}
               alt="avatar"
             />
             <AvatarFallback className="!rounded">
-              {AvatarData.AvatarName.slice(0, 2).toUpperCase()}
+              {AvatarData.name.slice(0, 2).toUpperCase()}
             </AvatarFallback>
           </Avatar>
         </div>
       );
     },
   },
-	{
-    accessorKey: "createDate",
-    header: "Created on",
-    cell: ({ row }) => {
-      const date = new Date(row.getValue("createDate"));
-      const formatted = date.toLocaleDateString();
-      return <div className="text-center font-medium">{formatted}</div>;
-    },
-  },
+  // {
+  //   accessorKey: "createDate",
+  //   header: "Created on",
+  //   cell: ({ row }) => {
+  //     const date = new Date(row.getValue("createDate"));
+  //     const formatted = date.toLocaleDateString();
+  //     return <div className="text-center font-medium">{formatted}</div>;
+  //   },
+  // },
   {
     id: "operations",
     header: "Operations",
@@ -66,9 +65,7 @@ export const columns: ColumnDef<AvatarDataProps>[] = [
           <Button
             className="py-2 px-4 bg-transparent border-none hover:bg-accent"
             onClick={() => {
-              console.log(
-                `Opening folder: ${AvatarData.AvatarName}`
-              );
+              console.log(`Opening folder: ${AvatarData.name}`);
             }}
           >
             <DeleteIcon fill="#1D3E80" height={23} width={23} />
