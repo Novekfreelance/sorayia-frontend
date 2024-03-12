b53f52c4f3fc2aa36e42461680a385b2c00f3ddb
SergeCodeur <amoussougboserge@gmail.com>
2024-03-12 21:08:59 +0100

    refactor :  changement de nom de variable et refactorisation de code

--- src/app/store/AuthStore.ts
+++ src/app/store/AuthStore.ts
@@ -2,29 +2,40 @@ import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 interface User {
-	id: string;
+  id: string;
   username: string;
   email: string;
-	password: string;
-	token: string;
-};
-
-interface AuthStore {
-	user: User | null;
-	setUser: (user: User) => void;
+  password: string;
 }
 
-const useUserStore = create(
-	persist <AuthStore>(
-		(set) => ({
-			user: null, // Initialize user as null
-			setUser: (user: User) => set({ user }),
-		}),
-		{ 
-			name: "auth",
-			storage: createJSONStorage(() => sessionStorage),
-		}
-	)
+interface Token {
+  token: string;
+}
+
+interface AuthStore {
+  user: User | null;
+  token: Token | null;
+  setUser: (user: User) => void;
+  setToken: (token: Token) => void;
+}
+
+const UserStore = create(
+  persist<AuthStore>(
+    (set) => ({
+      user: null, // Initialize user as null
+      token: null,
+      setUser: (user: User) => set({ user }),
+      setToken: (token: Token) => {
+        // Définir le cookie avec l'attribut HttpOnly
+        document.cookie = `accessToken=${token.token}; path=/; secure; HttpOnly`;
+        set({ token });
+      },
+    }),
+    {
+      name: "auth",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
 );
 
-export default useUserStore;
+export default UserStore;
