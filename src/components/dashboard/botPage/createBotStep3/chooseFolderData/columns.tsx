4d9714bd186c5dadcefa3fe8c0afa1f1b4412384
SergeCodeur <amoussougboserge@gmail.com>
2024-03-12 21:26:16 +0100

    feat : définition des tables pour afficher les dossiers lors du processus du création de bot

---
+++ src/components/dashboard/botPage/createBotStep3/chooseFolderData/columns.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ContentDataProps } from "@/app/(dashboard-folders)/dashboard/content/columns";
+import UseProcessContext from "@/app/store/CreateBotStore";
+import { Checkbox } from "@/components/ui/checkbox";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const columns: ColumnDef<ContentDataProps>[] = [
+  {
+    id: "select",
+    cell: ({ row }) => {
+      const { setFolders } = UseProcessContext();
+      const FolderData = row.original;
+      const handleChange = (value: boolean) => {
+        row.toggleSelected(!!value);
+        setFolders(FolderData);
+      };
+      return (
+        <Checkbox
+          className="h-6 w-6 border-none bg-gray-100 data-[state=checked]:bg-gray-100 data-[state=checked]:text-primary"
+          checked={row.getIsSelected()}
+          onCheckedChange={handleChange}
+          aria-label="Select row"
+        />
+      );
+    },
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "name",
+    header: "Title",
+  },
+];
