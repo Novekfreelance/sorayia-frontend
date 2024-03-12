7152e54ea9ce5fa81961c4625cf1f86e28f8aeff
SergeCodeur <amoussougboserge@gmail.com>
2024-03-12 21:29:57 +0100

    refactor : factorisation du code

--- src/components/dashboard/contentPage/AddNewFolderBtn.tsx
+++ src/components/dashboard/contentPage/AddNewFolderBtn.tsx
@@ -1,24 +1,28 @@
-"use client"
+"use client";
 
 import { FolderPlusIcon } from "@/components/icons/SvgIcons";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import NewForlderForm from "./NewFolderForm";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
+import NewForlderForm from "./NewFolderForm";
 
 const AddNewFolderBtn = () => {
+  const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false);
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button className={cn("gap-2")} size={"lg"}>
-					<FolderPlusIcon fill="#ffffff" width={24} height={24} />
-          Add new
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="max-w-2xl">
-        <NewForlderForm />
-      </DialogContent>
-    </Dialog>
+    <>
+      <Button
+        className={cn("gap-2")}
+        size={"lg"}
+        onClick={() => setShowAddEditNoteDialog(true)}
+      >
+        <FolderPlusIcon fill="#ffffff" width={24} height={24} />
+        Add new
+      </Button>
+      <NewForlderForm
+        open={showAddEditNoteDialog}
+        setOpen={setShowAddEditNoteDialog}
+      />
+    </>
   );
 };
 
