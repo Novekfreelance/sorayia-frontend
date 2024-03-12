f597b63be57e9d70bee5c26bc414e420e6d0139c
SergeCodeur <amoussougboserge@gmail.com>
2024-03-12 21:32:33 +0100

    refactor : factorisation du code

--- src/components/dashboard/profil/UserAvatar.tsx
+++ src/components/dashboard/profil/UserAvatar.tsx
@@ -1,19 +1,24 @@
 "use client";
+import UserStore from "@/app/store/AuthStore";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import useUserStore from "@/app/store/AuthStore";
-
+import { useEffect, useState } from "react";
 
 type UserAvatarProps = {
   className?: string;
 };
 
 const UserAvatar = ({ className }: UserAvatarProps) => {
-  const user = useUserStore((state) => state.user);
+  // this is used to prevent hydration error
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  const user = UserStore((state) => state.user);
   return (
     <Avatar className={`${className}`}>
       {/* <AvatarImage src="" /> */}
       <AvatarFallback>
-        {user ? (user.username.slice(0, 2).toUpperCase()): ""}
+        {isClient && <>{user?.username.slice(0, 2).toUpperCase()}</>}
       </AvatarFallback>
     </Avatar>
   );
