87f2157036f74411ab15b74bb0b514f4dbfc7fb7
SergeCodeur <amoussougboserge@gmail.com>
2024-03-12 21:33:24 +0100

    refactor : factorisation du code (ajout des information par défaut avant de recevoir les données de l'utilisateur

--- src/components/dashboard/profil/UserInfosCard.tsx
+++ src/components/dashboard/profil/UserInfosCard.tsx
@@ -1,21 +1,29 @@
 "use client";
+import UserStore from "@/app/store/AuthStore";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import SignOutBtn from "./SignOutBtn";
 import UserAvatar from "./UserAvatar";
-import useUserStore from "@/app/store/AuthStore";
 
 const UserInfosCard = () => {
-  const user = useUserStore((state) => state.user);
+  // this is used to prevent hydration error
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  const user = UserStore((state) => state.user);
   return (
     <div className="w-full flex justify-between items-center py-5 px-9 bg-primary">
       <div className="flex justify-center items-center gap-8">
         <UserAvatar className="w-40 h-40" />
         <div className="space-y-4">
-          {user && <p className="text-white text-3xl-600">{user.username}</p>}
+          {isClient && (
+            <p className="text-white text-3xl-600">{user?.username}</p>
+          )}
           <span className="text-xl-500 text-white block">
             Marketing manager
           </span>
-          {user && (
+          {isClient && (
             <Link
               href="#"
               className="underline underline-offset-2 text-base block text-white"
