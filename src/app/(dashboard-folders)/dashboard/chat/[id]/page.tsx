b8d91a4affbaa3a745006346c5bbbc56230824c8
SergeCodeur <amoussougboserge@gmail.com>
2024-03-12 20:58:30 +0100

    refactor : factorisation du code

--- src/app/(dashboard-folders)/dashboard/chat/[id]/page.tsx
+++ src/app/(dashboard-folders)/dashboard/chat/[id]/page.tsx
@@ -9,7 +9,7 @@ export type ConversationProps = {
     id: string;
   };
 };
-const Conversation: FC<ConversationProps> = ({ params: {id} }) => {
+const ConversationPage: FC<ConversationProps> = ({ params: { id } }) => {
   return (
     <div className="w-full h-full px-8 pt-8 overflow-y-scroll">
       <div className="flex justify-start items-center">
@@ -17,18 +17,18 @@ const Conversation: FC<ConversationProps> = ({ params: {id} }) => {
       </div>
       <div className="flex flex-col w-full h-[94%]">
         <div className="flex items-center justify-between mt-3 py-3 px-2 shadow rounded border-solid border border-shadowColor bg-white">
-          <h3 className="text-xl-500 text-primary">
-            Conversation{id}
-          </h3>
+          <h3 className="text-xl-500 text-primary">Conversation {id}</h3>
           <ConversationActionBtn />
         </div>
-        <div className="flex flex-col flex-1 w-full mt-3 px-2 overflow-hidden shadow rounded-t rounded-b-none border border-solid border-shadowColor bg-white">
-          <ChatContent chatID={id}/>
-          <ChatInput chatID={id} />
-				</div>
+        <div className="flex flex-col flex-1 h-full w-full mt-3 px-2 overflow-hidden shadow rounded border border-solid border-shadowColor bg-white relative">
+          <ChatContent chatID={id} />
+          <div className="absolute bottom-0 inset-x-0 pb-2 px-2">
+            <ChatInput chatID={id} />
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Conversation;
+export default ConversationPage;
