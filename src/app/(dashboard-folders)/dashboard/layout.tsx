1f7f14742bb0bcbc4a099bc5de4c7c3bbd2acde6
SergeCodeur <amoussougboserge@gmail.com>
2024-03-12 20:46:06 +0100

    feat : ajout du toast

--- src/app/(dashboard-folders)/dashboard/layout.tsx
+++ src/app/(dashboard-folders)/dashboard/layout.tsx
@@ -1,5 +1,7 @@
-import { ReactNode, FC } from "react";
+"use client";
 import Sidebar from "@/components/dashboard/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
+import { FC, ReactNode } from "react";
 
 type DashboardLayoutProps = {
   children: ReactNode;
@@ -11,8 +13,11 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
       <body>
         <main className="h-full-dvh flex w-full">
           <Sidebar />
-          <section className="flex-1 bg-gray-50 overflow-y-auto">{children}</section>
+          <section className="flex-1 bg-gray-50 overflow-y-auto">
+            {children}
+          </section>
         </main>
+        <Toaster />
       </body>
     </html>
   );
