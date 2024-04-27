93f5f53c42a8244c391545fc351a9c198c5a5bec
SergeCodeur <amoussougboserge@gmail.com>
2024-04-27 15:52:45 +0100

    refactor : mise à jour du bouton d'ajout de chat

--- src/components/dashboard/chatPage/AddNewYourConversationBtn.tsx
+++ src/components/dashboard/chatPage/AddNewYourConversationBtn.tsx
@@ -1,19 +1,27 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { PlusIcon } from "../../icons/SvgIcons";
 import { Button } from "../../ui/button";
+import AddYourConversationForm from "./createChatStep/AddYourConversationForm";
 
 const AddNewYourConversationBtn = () => {
-  const router = useRouter();
-  const handleClick = () => {
-    router.push("/dashboard/chat/create-chat");
-  };
+  const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false);
   return (
-    <Button className={cn("gap-1")} size={"lg"} onClick={handleClick}>
-      <PlusIcon width={25} height={25} fill="#ffffff" />
-      Add new
-    </Button>
+    <>
+      <Button
+        className={cn("gap-1")}
+        size={"lg"}
+        onClick={() => setShowAddEditNoteDialog(true)}
+      >
+        <PlusIcon width={25} height={25} fill="#ffffff" />
+        Add new
+      </Button>
+      <AddYourConversationForm
+        open={showAddEditNoteDialog}
+        setOpen={setShowAddEditNoteDialog}
+      />
+    </>
   );
 };
 
