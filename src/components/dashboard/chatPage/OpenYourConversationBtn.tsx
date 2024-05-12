1ae07adaa15d7be0f58bc70c77db0c2745ba50c3
SergeCodeur <amoussougboserge@gmail.com>
2024-05-12 19:57:17 +0100

    feat : ajout du nom chat dans un store

--- src/components/dashboard/chatPage/OpenYourConversationBtn.tsx
+++ src/components/dashboard/chatPage/OpenYourConversationBtn.tsx
@@ -1,22 +1,27 @@
 import { Button } from "@/components/ui/button";
+import useGetChatNameStore from "@/store/GetChatNameStore";
 import { useRouter } from "next/navigation";
 import { FC } from "react";
 
 type OpenYourConversationBtnProps = {
-	ChatId: string;
-}
-const OpenYourConversationBtn: FC<OpenYourConversationBtnProps> = ({ChatId}) => {
-	const router = useRouter();
-	return (
-		<Button
-          className="py-2 px-4 bg-primary"
-          onClick={() => {
-            router.push(`/dashboard/chat/${ChatId}`);
-          }}
-        >
-          Open
-        </Button>
-	)
-}
+  ChatId: string;
+  ChatName: string;
+};
+const OpenYourConversationBtn: FC<OpenYourConversationBtnProps> = ({
+  ChatId,
+  ChatName,
+}) => {
+  const router = useRouter();
+  const { setChatName } = useGetChatNameStore();
+  const handleClick = () => {
+    router.push(`/dashboard/chat/${ChatId}`);
+    setChatName(ChatName);
+  };
+  return (
+    <Button className="py-2 px-4 bg-primary" onClick={handleClick}>
+      Open
+    </Button>
+  );
+};
 
-export default OpenYourConversationBtn
+export default OpenYourConversationBtn;
