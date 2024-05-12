cdcb72026930bbad556ecd16705e15e1e8376773
SergeCodeur <amoussougboserge@gmail.com>
2024-05-12 19:57:42 +0100

    feat : ajout du nom de dossier dans le store de zustand

--- src/components/dashboard/contentPage/OpenFolderBtn.tsx
+++ src/components/dashboard/contentPage/OpenFolderBtn.tsx
@@ -1,25 +1,27 @@
 import { Button } from "@/components/ui/button";
+import useGetContentNameStore from "@/store/GetContentNameStore";
 import { useRouter } from "next/navigation";
 import { FC } from "react";
 
 type OpenFolderBtnProps = {
-	FolderId: string;
-}
-const OpenFolderBtn: FC<OpenFolderBtnProps> = ({FolderId}) => {
-	const router = useRouter();
-	return (
-		<Button
-          className="py-2 px-4 bg-primary"
-          onClick={() => {
-						console.log(
-							`Opening conversation: ${FolderId}`
-						);
-            router.push(`/dashboard/content/${FolderId}`);
-          }}
-        >
-          Open
-        </Button>
-	)
-}
+  FolderId: string;
+  FolderName: string;
+};
+const OpenFolderBtn: FC<OpenFolderBtnProps> = ({ FolderId, FolderName }) => {
+  const router = useRouter();
+  const { setContentName } = useGetContentNameStore();
+  return (
+    <Button
+      className="py-2 px-4 bg-primary"
+      onClick={() => {
+        console.log(`Opening conversation: ${FolderId}`);
+        router.push(`/dashboard/content/${FolderId}`);
+        setContentName(FolderName);
+      }}
+    >
+      Open
+    </Button>
+  );
+};
 
-export default OpenFolderBtn
+export default OpenFolderBtn;
